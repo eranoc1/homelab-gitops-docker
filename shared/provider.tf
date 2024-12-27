7c3ca2354480f717c3e021f70e39d693d75502c0
Eran Jian <eran.jian@googlemail.com>
2024-12-27 04:57:31 +0200

    Add docker provider for terraform

--- shared/provider.tf
+++ shared/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"  # Default Docker socket (use appropriate Docker endpoint)
 }
