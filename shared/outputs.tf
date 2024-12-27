2bec98a956744914837eb0ca6826ee425f30e81e
Eran Jian <eran.jian@googlemail.com>
2024-12-28 00:24:35 +0200

    Comment terraform output

--- shared/outputs.tf
+++ shared/outputs.tf
@@ -1,9 +1,9 @@
-output "container_name" {
-  value = docker_container.nginx.name
-  description = "The name of the Docker container"
-}
+# output "container_name" {
+#   value = docker_container.nginx.name
+#   description = "The name of the Docker container"
+# }
 
-output "container_ip" {
-  value = docker_container.nginx.ip_address
-  description = "The IP address of the Docker container"
-}
+# output "container_ip" {
+#   value = docker_container.nginx.ip_address
+#   description = "The IP address of the Docker container"
+# }
