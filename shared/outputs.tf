cb2a33e38bff316719e54f4f619f659011de6b88
Eran Jian <eran.jian@googlemail.com>
2024-12-28 00:33:22 +0200

    Update ip output

--- shared/outputs.tf
+++ shared/outputs.tf
@@ -4,6 +4,6 @@ output "container_name" {
 }
 
 output "container_ip" {
-  value       = docker_container.nginx.ip_address
+  value       = docker_container.nginx.network_data[0].ip_address
   description = "The IP address of the Docker container"
 }
