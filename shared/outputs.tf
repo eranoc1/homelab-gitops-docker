6ef1a84e8b0aae7f8fab0f4ec7e8196f6a3b545d
Eran Jian <eran.jian@googlemail.com>
2025-02-27 23:50:05 +0200

    Comment test containers output

--- shared/outputs.tf
+++ shared/outputs.tf
@@ -1,9 +1,9 @@
-output "container_name" {
-  value       = docker_container.nginx-gitops.name
-  description = "The name of the Docker container"
-}
+# output "container_name" {
+#   value       = docker_container.nginx-gitops.name
+#   description = "The name of the Docker container"
+# }
 
-output "container_ip" {
-  value       = docker_container.nginx-gitops.network_data[0].ip_address
-  description = "The IP address of the Docker container"
-}
+# output "container_ip" {
+#   value       = docker_container.nginx-gitops.network_data[0].ip_address
+#   description = "The IP address of the Docker container"
+# }
