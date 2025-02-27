200ad54d413ee7ae64a9833f203914848408b3f9
Eran Jian <eran.jian@googlemail.com>
2025-02-28 01:56:28 +0200

    Try to fix cloudflared terraform config

--- app1/main.tf
+++ app1/main.tf
@@ -1,20 +1,12 @@
 # module "provider" {
 #   source = "../shared"
 # }
-resource "docker_container" cloudflared {
+resource "docker_container" "cloudflared" {
   name  = var.container_name 
   image = var.image_name
-  # command = var.command
-  # env = {
-  #   CLOUDFLARED_TOKEN = var.cloudflared_token
-  # }
-
-  command = "${var.command} ${var.cloudflared_token}"
-  env = {
-    CLOUDFLARED_TOKEN = var.cloudflared_token
-  }    
-#   ports {
-#     internal = 
-#     external = 
-#   }
+  command = ["${var.command}", "${var.cloudflared_token}"]
+  env = [
+    "CLOUDFLARED_TOKEN=${var.cloudflared_token}"
+  ]
+  # other configurations...
 }
