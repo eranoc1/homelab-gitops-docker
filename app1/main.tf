f72ed9f9ff45e139f8f2d286995eb64a6094e860
Eran Jian <eran.jian@googlemail.com>
2025-02-28 02:09:58 +0200

    Try to fix cloudflared terraform config

--- app1/main.tf
+++ app1/main.tf
@@ -1,12 +1,32 @@
-# module "provider" {
-#   source = "../shared"
-# }
+variable "image_name" {
+  description = "The Docker image to use"
+  type        = string
+  default     = "cloudflare/cloudflared:2025.2.1-arm64"
+}
+
+variable "container_name" {
+  description = "The name of the Docker container"
+  type        = string
+  default     = "cloudflared"
+}
+
+variable "cloudflared_token" {
+  description = "cloudflared token"
+  type        = string
+}
+
+variable "command" {
+  description = "Docker container command"
+  type        = string
+  default     = "tunnel --no-autoupdate run --token"
+}
+
+locals {
+  full_command = "${var.command} ${var.cloudflared_token}"
+}
+
 resource "docker_container" "cloudflared" {
   name    = var.container_name
   image   = var.image_name
-  command = split(" ", local.full_command)
-  env = [
-    "CLOUDFLARED_TOKEN=${var.cloudflared_token}"
-  ]
-  # other configurations...
+  command = var.full_command
 }
