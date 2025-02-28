a31df70ec5704aeeaf6ae1636c7c22f22988a960
Eran Jian <eran.jian@googlemail.com>
2025-02-28 02:17:17 +0200

    Revert "Try to fix cloudflare terraform config"
    This reverts commit 910ca45040559b96c480a1fd2540775d830f3484.

--- app1/variables.tf
+++ app1/variables.tf
@@ -1,4 +1,3 @@
-// filepath: /Users/eran/Code/homelab-gitops/app1/variables.tf
 variable "image_name" {
   description = "The Docker image to use"
   type        = string
@@ -10,26 +9,19 @@ variable "container_name" {
   type        = string
   default     = "cloudflared"
 }
-
 variable "cloudflared_token" {
   description = "cloudflared token"
   type        = string
+  default     = ""
 }
-<<<<<<< HEAD
-=======
 # variable "command" {
 #   description = "Docker container command"
 #   type        = string
 #   default     = "tunnel --no-autoupdate run --token ${var.cloudflared_token}"
 # }
->>>>>>> 79bc897 (Try to fix cloudflared docker image)
 
 variable "command" {
   description = "Docker container command"
   type        = string
   default     = "tunnel --no-autoupdate run --token"
-}
-
-locals {
-  full_command = "${var.command} ${var.cloudflared_token}"
 }
