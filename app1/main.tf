bc381e7d55bc43350dd536d32fcde4614f81e608
Eran Jian <eran.jian@googlemail.com>
2025-02-28 02:27:35 +0200

    Test cloudflared terraform command

--- app1/main.tf
+++ app1/main.tf
@@ -4,5 +4,5 @@
 resource "docker_container" "cloudflared" {
   name  = var.container_name 
   image = var.image_name
-  command = concat(split(" ", var.command), [var.cloudflared_token])
+  command = var.command
 }
