606fe005502b45eab56b887abc8fa3d98ed3fa67
Eran Jian <eran.jian@googlemail.com>
2025-02-28 02:25:23 +0200

    Try to fix cloudflared terraform command

--- app1/main.tf
+++ app1/main.tf
@@ -4,5 +4,5 @@
 resource "docker_container" "cloudflared" {
   name  = var.container_name 
   image = var.image_name
-  command = ["sh", "-c", "${var.command} ${var.cloudflared_token}"]
+  command = "${var.command} ${var.cloudflared_token}"
 }
