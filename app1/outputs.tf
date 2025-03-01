aa7b63c5812fea93856dc0ec660361fc76550ff6
Eran Jian <eran.jian@googlemail.com>
2025-03-01 02:14:01 +0200

    Update outputs.tf

--- app1/outputs.tf
+++ app1/outputs.tf
@@ -1,9 +1,9 @@
 output "container_name" {
-  value       = docker_container.var.container_name.name
+  value       = docker_container.var.cloudflared.name
   description = "The name of the Docker container"
 }
 
 output "container_ip" {
-  value       = docker_container.var.container_name.network_data[0].ip_address
+  value       = docker_container.var.cloudflared.network_data[0].ip_address
   description = "The IP address of the Docker container"
 }
