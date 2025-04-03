90f21f4ba852bac36259a252f7d3d2b886e9bc9a
Eran Jian <eran.jian@googlemail.com>
2025-04-03 22:01:40 +0300

    Add transmission module to main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "cloudflared" {
   source = "./cloudflared"
 }
+module "transmission" {
+   source = "./transmission"
+ }
+module "shared" {
+  source = "./shared"
+}
+
 # variable "tunnel_token" {
 #   description = "Token for the tunnel"
 #   type        = string
 #   default     = ""
 # }
 
-# module "app2" {
-#   source = "./app2"
-# }
-
-module "shared" {
-  source = "./shared"
-}
