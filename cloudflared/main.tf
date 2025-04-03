c1ee0095dd73a659c72c32f65ebd26b9a6bafe84
Eran Jian <eran.jian@googlemail.com>
2025-04-03 23:12:03 +0300

    Disable removing temp var file

--- cloudflared/main.tf
+++ cloudflared/main.tf
@@ -1,11 +1,7 @@
-# module "provider" {
-#   source = "../shared"
-# }
 resource "docker_container" "cloudflared" {
   name  = var.container_name 
   image = var.image_name
   network_mode = var.network_mode
-  #command = var.command
   command = [
     "tunnel",
     "--no-autoupdate",
