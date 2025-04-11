0ba19b5db40e3586ff1a2d7d25a00f4e97ec801e
Eran Jian <eran.jian@googlemail.com>
2025-04-12 01:42:40 +0300

    Correct plex output values

--- plex/outputs.tf
+++ plex/outputs.tf
@@ -1,9 +1,9 @@
 output "container_name" {
-  value       = docker_container.transmission.name
+  value       = docker_container.plex.name
   description = "The name of the Docker container"
 }
 
 output "container_ip" {
-  value       = docker_container.transmission.network_data[0].ip_address
+  value       = docker_container.plex.network_data[0].ip_address
   description = "The IP address of the Docker container"
 }
