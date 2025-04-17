96021889f708decaa6e7dc927c844319fb9dde13
eran101 <eran.jian@googlemail.com>
2025-04-18 00:54:43 +0300

    Comment shared modules code

--- shared/variables.tf
+++ shared/variables.tf
@@ -1,14 +1,14 @@
-variable "image_name" {
-  description = "The Docker image to use"
-  type        = string
-  default     = "nginx:stable-alpine"
-}
+# variable "image_name" {
+#   description = "The Docker image to use"
+#   type        = string
+#   default     = "nginx:stable-alpine"
+# }
 
-variable "container_name" {
-  description = "The name of the Docker container"
-  type        = string
-  default     = "nginx-gitops"
-}
+# variable "container_name" {
+#   description = "The name of the Docker container"
+#   type        = string
+#   default     = "nginx-gitops"
+# }
 
 # variable "tunnel_token" {
 #   description = "tunnel token"
